cd070c51f81159166c6e12fc142fb9b6fe80c7b6
Spencer Austman <spencerlgaustman@gmail.com>
2017-02-19 19:15:27 -0800

    Adjusted the bot for the /r/UBC Discord server and added rate limit.

--- src/roleBot.js
+++ src/roleBot.js
@@ -1,61 +1,4 @@
-// var Discord = require('discord.io');
-// var bot = new Discord.Client({
-//     autorun: true,
-//     token: "MjgyMDE0NDEyMTU4Nzk1Nzg2.C4kROQ.p6nFyT7rpc5QFEvz3c53ULFrMaw"
-// });
-//
 
-//
-// bot.on('ready', function(event) {
-//     console.log('Logged in as %s - %s\n', bot.username, bot.id);
-//     bot.sendMessage({
-//         to: "282277372609429504",
-//         message: "Hello everyone"
-//     });
-// });
-//
-// bot.on('message', function(user, userID, channelID, message, event) {
-//     var parsedMessage = message.split(" ");
-//     if (parsedMessage[0] === "!ping") {
-//         bot.sendMessage({
-//             to: channelID,
-//             message: parsedMessage[1]
-//         });
-//     }
-//     if (parsedMessage[0] === "!role") {
-//         bot.addToRole({"serverID":channelID,"userID":userID,"roleID":roleIDs[parsedMessage[1]]},function(err,response) {
-//             if (err) console.error(err); /* Failed to apply role */
-//             /* some code */
-//         });
-//         bot.sendMessage({
-//             to: channelID,
-//             message: "role Added "
-//         });
-//     }
-// });
-/*
- A ping pong bot, whenever you send "ping", it replies "pong".
- */
-
-var yearIDs = {
-    test: "282302608524443658",
-    please: "282308075438866433",
-    alumni: "282361501715791872",
-    first: "282361260417351680",
-    second: "282361312804339722",
-    third: "282361318546210817",
-    fourth: "282361321775824896",
-    fifth: "282361323545821184",
-    sixth: "282361471898353664",
-    seventh: "282361488424042498"
-};
-
-var programIDs = {
-    sauder: "282382735882977290",
-    science: "282382712642338817"
-};
-
-var currentUserID;
 // import the discord.js module
 const Discord = require('discord.js');
 
@@ -66,30 +9,61 @@ const bot = new Discord.Client();
 const token = 'MjgyMDE0NDEyMTU4Nzk1Nzg2.C4kROQ.p6nFyT7rpc5QFEvz3c53ULFrMaw';
 
 
+var yearIDs = {
+    alumni: "280085913382551554",
+    first: "275881487738601475",
+    second: "275881588137525248",
+    third: "275881602154758144",
+    fourth: "275881611680153600",
+    fifth: "275881621507538945",
+    sixth: "275881638771294208",
+    seventh: "275881649294671872"
+};
 
+var programIDs = {
+    arts: "275877649199661056",
+    architecture: "276145995782422528",
+    biospsychology: "280104705932394496",
+    computerscience: "275897810564415488",
+    engineering: "275877766753288192",
+    kinesiology: "275878916693164032",
+    lfs: "275881139170836481",
+    music: "275881225967632385",
+    pharmacy: "276241227102289920",
+    poliscience: "279147501892796416",
+    sauder: "275877687594057729",
+    science: "275877745052090370"
+};
+
+var userBase = {
+    test: 0
+};
 
 // the ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted.
 bot.on('ready', () => {
     console.log('I am ready!');
+    bot.channels.get("278002373803114496").sendMessage("Hello everyone, I'm back!");
 });
 // create an event listener for messages
 bot.on('message', message => {
 // filter for user response
     const filter = message => message.content.match(/^\d+$/);
-
+// 282277372609429504 <- mine
+//  282389811275497473 <- ubc
 if (message.content[0] === '!') {
+    // if (message.author.id === "228349219700736001"){
+    //    message.reply("Sorry, I'm going to have to transfer you to Douglas College");
+    //     return;
+    // }
     var parsedMessage = message.content.split(" ");
     switch (parsedMessage[0]) {
         case '!year': {
             // Check if user can set role
             message.reply("OK, let's give you a role! What year are you in? \n(Type 0 for Alumni or a number from 1 to 7 corresponding to your standing.)");
-            //  var channel = bot.channels.get("282277372609429504");
-            //  console.log(channel);
-            bot.channels.get("282277372609429504").awaitMessages(filter, {max: 1, time: 30000, errors: ['time']})
+            bot.channels.get("278002373803114496").awaitMessages(filter, {max: 1, time: 60000, errors: ['time']})
                 .then(collected => {
-                assignYear(collected.first().toString(), message);
-            message.reply("If you want your program/faculty added to your user, please user the !program command.")})
+                assignYear(collected.first().toString(), message);})
         .catch(collected => message.reply(`You didn't respond, did you end up going to SFU? \nIn any case, I'm returning to standby.`));
             break;
         }
@@ -98,13 +72,16 @@ if (message.content[0] === '!') {
                 "\n You can choose from the following, simply type the correspond number: " +
                 "\n 1) Arts \n 2) Architecture \n 3) Biopsychology \n 4) Computer Science \n 5) Engineering \n 6) Kinesiology \n 7) LFS \n 8) Music" +
                 "\n 9) Pharmacy \n 10) Political Science \n 11) Sauder \n 12) Science \n 13) My faculty/program isn't listed");
-            bot.channels.get("282277372609429504").awaitMessages(filter, {max: 1, time: 30000, errors: ['time']})
+            bot.channels.get("278002373803114496").awaitMessages(filter, {max: 1, time: 60000, errors: ['time']})
                 .then(collected => assignProgram(collected.first().toString(), message))
         .catch(collected => message.reply(`You didn't respond, did you end up going to Langara? \nIn any case, I'm returning to standby.`));
             break;
         }
         case '!help':
-            message.reply("Currently only the !year and !program commands are supported, bug Namtsua if you want another feature to be added.");
+            message.reply("Currently only the !year, !program and !ams commands are supported, bug Namtsua if you want another feature to be added.");
+            break;
+        case '!ams':
+            message.reply("http://streamable.com/gbqjv");
             break;
         default:
             message.reply("Sorry, I don't recognize that command. Please use !help to view all valid commands.")
@@ -112,10 +89,12 @@ if (message.content[0] === '!') {
 }});
 
 function assignYear(year, message){
+    if (checkLimit(message.author.id, message))
+        return;
     switch(year){
         case '0':
             message.reply("What are you doing here? Just kidding, anyone UBC-related is welcome.");
-            message.member.addRole(yearIDs['alumni']);
+            message.member.addRole("282302608524443658");
             break;
         case '1':
             message.reply("Welcome to UBC! I hpe you are enjoying your first year.");
@@ -149,10 +128,12 @@ function assignYear(year, message){
             message.reply("Sorry, please input a value from 0 to 7");
             break;
     }
+    message.reply("If you want your program/faculty added to your user, please user the !program command.")
 }
 
 function assignProgram(program, message){
-    console.log('hi');
+    if (checkLimit(message.author.id, message))
+        return;
     switch(program){
         case '1':
             message.reply("Arts, eh? Tired of writing essays yet?");
@@ -208,6 +189,16 @@ function assignProgram(program, message){
         default:
             message.reply("Sorry, please input a value from 1 to 13.");
     }
+}
+
+function checkLimit(userID, message){
+    var currentDate = Date.now();
+    if (userBase.hasOwnProperty(userID) && (currentDate - userBase[userID]  <= 3600000)){
+        message.reply("Sorry, you've hit your limit. Hope you're Santasfied! \n Please try again in " + Math.floor((3600000 - (currentDate - userBase[userID]))/60000) + " minutes.");
+        return true;
+    }else
+        userBase[userID] = currentDate;
+    return false;
 }
 
 // log our bot in
